13e5a497eee6b601ee11f7019f4d37a1e88aea15
notalentgeek <mikael.pratama@gmail.com>
2025-04-17 13:53:38 +0700

    Added Quality of Life Improvement on admin/js/plugin-reporter-admin.js

--- admin/js/plugin-reporter-admin.js
+++ admin/js/plugin-reporter-admin.js
@@ -1,14 +1,37 @@
 /**
- * All of the JavaScript for your admin-specific functionality should be
- * included in this file.
+ * Plugin Reporter Admin JavaScript
+ *
+ * All of the JavaScript for your admin-specific functionality.
+ *
+ * @package    Plugin_Reporter
+ * @subpackage Plugin_Reporter/admin/js
  */
 
 (function( $ ) {
     'use strict';
 
+    /**
+     * Initialize the admin functionality when the DOM is ready.
+     */
     $(document).ready(function() {
-        // Add any JavaScript functionality here
-        console.log('Plugin Reporter loaded');
+        // Tab functionality
+        $('.plugin-reporter-tabs .nav-tab').on('click', function(e) {
+            e.preventDefault();
+
+            // Hide all tabs
+            $('.tab-content').hide();
+
+            // Show the selected tab
+            $($(this).attr('href')).show();
+
+            // Update active tab class
+            $('.nav-tab').removeClass('nav-tab-active');
+            $(this).addClass('nav-tab-active');
+        });
+
+        // Show the first tab by default
+        $('#all-plugins').show();
+        $('#needs-update').hide();
     });
 
 })( jQuery );
