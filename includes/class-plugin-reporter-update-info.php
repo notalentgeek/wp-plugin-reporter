6071b91dcebd11650a4e2499f4f64daa5fdfb96e
notalentgeek <mikael.pratama@gmail.com>
2025-04-17 11:05:14 +0700

    Added Quality of Life Improvement on includes/class-plugin-reporter-update-info.php

--- includes/class-plugin-reporter-update-info.php
+++ includes/class-plugin-reporter-update-info.php
@@ -2,6 +2,9 @@
 /**
  * The file that defines the update information functionality
  *
+ * @link       https://github.com/notalentgeek/wp-plugin-reporter
+ * @since      1.0.0
+ *
  * @package    Plugin_Reporter
  * @subpackage Plugin_Reporter/includes
  */
@@ -11,20 +14,26 @@
  *
  * This is used to collect plugin update information.
  *
+ * @since      1.0.0
  * @package    Plugin_Reporter
  * @subpackage Plugin_Reporter/includes
+ * @author     New Naratif Development Team <tech@newnaratif.com>
  */
 class Plugin_Reporter_Update_Info {
 
     /**
      * WordPress update data
      *
-     * @var object
+     * @since    1.0.0
+     * @access   private
+     * @var      object    $update_data    The WordPress update data.
      */
     private $update_data;
 
     /**
      * Constructor
+     *
+     * @since    1.0.0
      */
     public function __construct() {
         // Initialize update data
@@ -34,7 +43,8 @@ class Plugin_Reporter_Update_Info {
     /**
      * Simple test method
      *
-     * @return string A test message
+     * @since    1.0.0
+     * @return   string    A test message
      */
     public function test_method() {
         return 'Update info class is working';
@@ -43,7 +53,8 @@ class Plugin_Reporter_Update_Info {
     /**
      * Refresh the WordPress update data
      *
-     * @return void
+     * @since    1.0.0
+     * @return   void
      */
     public function refresh_update_data() {
         // Force WordPress to check for plugin updates
@@ -56,10 +67,11 @@ class Plugin_Reporter_Update_Info {
     /**
      * Get update information for a specific plugin
      *
-     * @param string $plugin_file The plugin file path relative to the plugins directory
-     * @return array Update information for the plugin
+     * @since    1.0.0
+     * @param    string    $plugin_file    The plugin file path relative to the plugins directory
+     * @return   array     Update information for the plugin
      */
-    public function get_plugin_update_info($plugin_file) {
+    public function get_plugin_update_info( $plugin_file ) {
         // Default update information
         $update_info = array(
             'update_available' => false,
@@ -68,20 +80,20 @@ class Plugin_Reporter_Update_Info {
         );
 
         // Ensure we have access to WordPress plugin functions
-        if (!function_exists('get_plugin_data')) {
+        if ( ! function_exists( 'get_plugin_data' ) ) {
             require_once ABSPATH . 'wp-admin/includes/plugin.php';
         }
 
         // Get plugin data
-        $plugin_data = get_plugin_data(WP_PLUGIN_DIR . '/' . $plugin_file);
-        $update_info['current_version'] = isset($plugin_data['Version']) ? $plugin_data['Version'] : '';
+        $plugin_data = get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin_file );
+        $update_info['current_version'] = isset( $plugin_data['Version'] ) ? $plugin_data['Version'] : '';
 
         // Check if there's an update available
-        if (isset($this->update_data->response[$plugin_file])) {
+        if ( isset( $this->update_data->response[$plugin_file] ) ) {
             $update = $this->update_data->response[$plugin_file];
 
             $update_info['update_available'] = true;
-            $update_info['latest_version'] = isset($update->new_version) ? $update->new_version : '';
+            $update_info['latest_version'] = isset( $update->new_version ) ? $update->new_version : '';
         } else {
             // No update available, so latest version is the current version
             $update_info['latest_version'] = $update_info['current_version'];
